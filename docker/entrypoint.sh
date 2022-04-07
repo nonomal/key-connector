c27cb7ae8ad763a0e9eb9a00dc2ff2e49631ec63
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2022-04-07 16:31:38 -0400

    Add certificate generation to Dockerfile entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+if [ ! -f /etc/bitwarden/key-connector/bwkc.pfx ]; then
+  openssl req \
+  -x509 \
+  -newkey rsa:4096 \
+  -sha256 \
+  -nodes \
+  -keyout bwkc.key \
+  -out bwkc.crt \
+  -subj "/CN=Bitwarden Key Connector" \
+  -days 36500
+
+  openssl pkcs12 \
+  -export \
+  -out /etc/bitwarden/key-connector/bwkc.pfx \
+  -inkey bwkc.key \
+  -in bwkc.crt \
+  -passout pass:$keyConnectorSettings__certificate__filesystemPassword
+
+  rm bwkc.crt
+  rm bwkc.key
+fi
+
 cp /etc/bitwarden/ca-certificates/*.crt /usr/local/share/ca-certificates/ >/dev/null 2>&1 \
     && update-ca-certificates
 
